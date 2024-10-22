96e19b71468a487605d40f0958d3291dc4e3c35d
ceca5556 <ceca5556@colorado.edu>
2024-10-21 22:40:54 -0600

    added elemets to the aesd_dev struct

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -8,6 +8,8 @@
 #ifndef AESD_CHAR_DRIVER_AESDCHAR_H_
 #define AESD_CHAR_DRIVER_AESDCHAR_H_
 
+#include "aesd-circular-buffer.h"
+
 #define AESD_DEBUG 1  //Remove comment on this line to enable debug
 
 #undef PDEBUG             /* undef it, just in case */
@@ -27,8 +29,12 @@ struct aesd_dev
 {
     /**
      * TODO: Add structure(s) and locks needed to complete assignment requirements
-     */
+    */
     struct cdev cdev;     /* Char device structure      */
+    struct aesd_circular_buffer circ_buf; // circular buffer
+    struct aesd_buffer_entry *buf_entry; // working entry value 
+    struct mutex buf_lock; // lock for device writes
+
 };
 
 
